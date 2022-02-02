8ad79302a69255634a553af34b8798536dec9c0f
marianarmorgado <marianarmorgado@gmail.com>
2022-02-02 16:35:12 +0200

    copying lists

---
+++ sample/foods.py
@@ -0,0 +1,5 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:] # copy a list by slicing the original list without boundaries
+
+print("My favourite foods are:")
+print(my_foods)
