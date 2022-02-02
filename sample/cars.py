3b204bd806a105648392234e9981a64879be9096
marianarmorgado <marianarmorgado@gmail.com>
2022-02-02 15:22:23 +0200

    organize lists

---
+++ sample/cars.py
@@ -0,0 +1,27 @@
+from pickle import TRUE
+
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort() # sorts list by alphabetical order
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse = True) # sorts list by reverse alphabetical order
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHereis the original list again:")
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.reverse() # print in reverse order
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+len(cars) # length of a list
