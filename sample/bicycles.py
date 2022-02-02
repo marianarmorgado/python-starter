6be84229152b3f54389361c85325dcb5345d5615
marianarmorgado <marianarmorgado@gmail.com>
2022-02-02 12:08:33 +0200

    create lists

---
+++ sample/bicycles.py
@@ -0,0 +1,8 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized'] #creates list
+print(bicycles)
+print(bicycles[0]) # prints first item of list
+print(bicycles[0].title())
+print(bicycles[-1]) #prints last item of list
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
