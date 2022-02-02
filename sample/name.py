0c61fef0a6d980ec9b6d5f4f0a0713cb1dbb2c35
marianarmorgado <marianarmorgado@gmail.com>
2022-02-02 11:26:21 +0200

    first scripts

---
+++ sample/name.py
@@ -0,0 +1,18 @@
+name = "ada lovelace"
+print(name.title()) #title() makes first letters with uppercase
+print(name.upper()) #upper() makes all letters uppercase
+print(name.lower()) #lower() makes all letters lowercase
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+print("Hello, " + full_name.title() + "!")
+
+favourite_language = "python "
+favourite_language.rstrip() # to exclude white spaces
+favourite_language = favourite_language.rstrip() # to exclude white spaces and store it
+favourite_language
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+message
