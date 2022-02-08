d2829b784d434a69fed2ef74e94923e6c557a836
marianarmorgado <marianarmorgado@gmail.com>
2022-02-08 10:55:48 +0200

    simple if then + check for equality

--- sample/cars.py
+++ sample/cars.py
@@ -1,4 +1,5 @@
 from pickle import TRUE
+from sqlite3 import enable_shared_cache
 
 
 cars = ['bmw', 'audi', 'toyota', 'subaru']
@@ -23,5 +24,14 @@ cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.reverse() # print in reverse order
 print(cars)
 
-cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars = ['audi', 'bmw', 'subaru', 'toyota']
 len(cars) # length of a list
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+car = 'bmw' # to set the value of car to 'bmw'
+car == 'bmw' # to check whether this is true (checking for equality)
