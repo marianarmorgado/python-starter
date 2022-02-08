47ea150db05fe64d9e21ee500fb29d0fcf97800d
marianarmorgado <marianarmorgado@gmail.com>
2022-02-08 13:45:17 +0200

    checking for special values

--- sample/toppings.py
+++ sample/toppings.py
@@ -23,3 +23,20 @@ if 'extra vegan cheese' in requested_toppings:
     print("Adding extra vegan cheese.")
 
 print("\nFinished making your pizza!")
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra vegan cheese']
+
+for requested_topping in requested_toppings:
+    print("Adding " + requested_topping + ".")
+
+print("\nFinished making yout pizza!")
+
+# if there are no green peppers
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right.")
+    else:
+        print("Adding " + requested_topping + ".")
+
+print("\nFinished making yout pizza!")
