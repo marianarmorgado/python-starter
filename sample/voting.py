e902b8441e74c3c2997f3f9a2ed487c0c6ba6b00
marianarmorgado <marianarmorgado@gmail.com>
2022-02-08 12:55:27 +0200

    if else statements

---
+++ sample/voting.py
@@ -0,0 +1,10 @@
+from doctest import ELLIPSIS_MARKER
+
+
+age = 19
+if age >= 18:
+    print("You are olf enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as you turn 18!")
