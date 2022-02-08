084775f1af67774c71f6e0ddeaf719c9891e4722
marianarmorgado <marianarmorgado@gmail.com>
2022-02-08 13:39:22 +0200

    testing multiple conditions

--- sample/toppings.py
+++ sample/toppings.py
@@ -10,3 +10,16 @@ requested_toppings = ['mushrooms', 'onions', 'pinapple']
 topping = 'garlic'
 if topping not in requested_toppings:
     print(topping.title() + ", you are not a topping!")
+
+requested_toppings = ['mushrooms', 'extra vegan cheese']
+
+# testing multiple conditions
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepper' in requested_toppings:
+    print("Adding peppers.")
+if 'extra vegan cheese' in requested_toppings:
+    print("Adding extra vegan cheese.")
+
+print("\nFinished making your pizza!")
