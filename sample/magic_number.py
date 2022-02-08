0b287a289dde6744c2b6c5d2db23cb51d6b7e15e
marianarmorgado <marianarmorgado@gmail.com>
2022-02-08 12:47:53 +0200

    numerical conditions

---
+++ sample/magic_number.py
@@ -0,0 +1,28 @@
+answer = 17
+
+if answer != 42:
+    print("That is not the correct answer. Please try again!")
+
+age = 19
+
+# numerical comparisons
+age < 21
+age <= 21
+age > 21
+age >= 21
+
+# checking multiple conditions
+age_0 = 22
+age_1 = 18
+age_0 >= 21 and age_1 >= 21
+
+age_1 = 22
+age_0 >= 21 and age_1 >=21
+
+(age_0 >= 21) and (age_1 >=21) # to improve readibility
+
+age_0 = 22
+age_1 = 18
+(age_0 >= 21) or (age_1 >= 21)
+age_0 = 18
+(age_0 >= 21) or (age_1 >= 21)
