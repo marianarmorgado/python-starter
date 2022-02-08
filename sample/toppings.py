0bc7b81705c59c6543af647acd6c1e876987d272
marianarmorgado <marianarmorgado@gmail.com>
2022-02-08 12:52:15 +0200

    checking if value is in list

--- sample/toppings.py
+++ sample/toppings.py
@@ -2,3 +2,11 @@ requested_topping = 'mushrooms'
 
 if requested_topping != 'onions': #checking for inequality
     print("Hold the onions")
+
+requested_toppings = ['mushrooms', 'onions', 'pinapple']
+'mushrooms' in requested_toppings # checking if a value is in a list
+'garlic' in requested_toppings
+
+topping = 'garlic'
+if topping not in requested_toppings:
+    print(topping.title() + ", you are not a topping!")
