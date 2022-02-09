9dc9a093d8ad834cc52f9459fbf95ec0f5a070b5
marianarmorgado <marianarmorgado@gmail.com>
2022-02-09 16:46:27 +0200

    dictionary of similar things

---
+++ sample/languages.py
@@ -0,0 +1,14 @@
+favourite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+print(
+    "Sarah's favourite language is " +
+    favourite_languages['sarah'].title() +
+    "."
+)
+
+favourite_languages['sarah']
