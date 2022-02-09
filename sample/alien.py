ba5c36a1d698e783024842b03976a31a975af14e
marianarmorgado <marianarmorgado@gmail.com>
2022-02-09 16:43:15 +0200

    dictionaries

---
+++ sample/alien.py
@@ -0,0 +1,51 @@
+alien_0 = {'color': 'green', 'points': 5} #create a dictionary
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+new_points = alien_0['points']
+print("You just earned " + str(new_points) + " points!")
+
+print(alien_0)
+
+alien_0['x_position'] = 0 # add to the dictionary
+alien_0['y_position'] = 25 # add to the dictionary
+
+print(alien_0)
+
+# starting with an empty dictionary
+
+alien_0 = {}
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
+
+# modifying values in a dictionary
+
+alien_0 = {'color': 'green'}
+print("The alien is " + alien_0['color'] + ".")
+
+alien_0 = {'color': 'yellow'}
+print("The alien is now " + alien_0['color'] + ".")
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("Original x-position: " + str(alien_0['x_position']))
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print("New x-position: " + str(alien_0['x_position']))
+
+# removing key value pairs
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
