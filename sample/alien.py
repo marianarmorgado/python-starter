0a2da4a87c6fe88729235f81ec73d63d62fbb2a1
marianarmorgado <marianarmorgado@gmail.com>
2022-02-09 17:36:23 +0200

    list of dictionary

--- sample/alien.py
+++ sample/alien.py
@@ -49,3 +49,42 @@ print(alien_0)
 
 del alien_0['points']
 print(alien_0)
+
+# list of dictionaries
+
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
+
+# make the list of 30 aliens
+
+aliens = []
+
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+# show the first 5 aliens
+
+for alien in aliens [:5]:
+    print(alien)
+print("...")
+
+# show how many aliens have been created
+
+print("Total number of aliens: " + str(len(aliens)))
+
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] == 'yellow'
+        alien['speed'] == 'medium'
+        alien['points'] == 10
+
+for alien in aliens[0:5]:
+    print()
+print("...")
