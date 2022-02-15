7f8b827762afa5c342af0c8e9305c854b7de5080
marianarmorgado <marianarmorgado@gmail.com>
2022-02-15 10:14:59 +0200

    list of dictionaries

--- sample/alien.py
+++ sample/alien.py
@@ -86,5 +86,19 @@ for alien in aliens[0:3]:
         alien['points'] == 10
 
 for alien in aliens[0:5]:
-    print()
+    print(alien)
+print("...")
+
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] == 'yellow'
+        alien['speed'] == 'medium'
+        alien['points'] == 10
+    elif alien['color'] == 'yellow':
+        alien['color'] == 'red'
+        alien['speed'] == 'fast'
+        alien['points'] == 15
+
+for alien in aliens[0:5]:
+    print(alien)
 print("...")
